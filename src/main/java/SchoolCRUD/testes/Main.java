c0d70e495dff168243521085651c34624b3e8f0b
https://github.com/ariadnerezende <ariadnederezende@gmail.com>
2024-03-09 16:23:22 -0300

    menu creation and testing H2

--- src/main/java/SchoolCRUD/testes/Main.java
+++ src/main/java/SchoolCRUD/testes/Main.java
@@ -1,27 +1,11 @@
 package SchoolCRUD.testes;
-
-import SchoolCRUD.dao.AlunoDao;
-import SchoolCRUD.modelo.Aluno;
-import SchoolCRUD.util.JPAUtil;
-import jakarta.persistence.EntityManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.math.BigDecimal;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
-
-        EntityManager em = JPAUtil.getEntityManager();
-        AlunoDao alunoDao = new AlunoDao(em);
-        Aluno a1 = new Aluno("Ariadne", "SC3034437", "a@gmail.com", new BigDecimal(10), new BigDecimal(9), new BigDecimal(10));
-
-
-        em.getTransaction().begin();
-        alunoDao.cadastrar(a1);
-        em.getTransaction().commit();
-        em.close();
+        Menu.chamarMenu();
     }
 }
---
+++ src/main/java/SchoolCRUD/testes/Menu.java
@@ -0,0 +1,107 @@
+package SchoolCRUD.testes;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import SchoolCRUD.dao.AlunoDao;
+import SchoolCRUD.modelo.Aluno;
+import SchoolCRUD.util.JPAUtil;
+
+public class Menu {
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("School-CRUD");
+
+    private static final EntityManager entityManager = entityManagerFactory.createEntityManager();
+    private static final AlunoDao alunoDao = new AlunoDao(entityManager);
+
+    public static void chamarMenu() {
+        boolean continuar = true;
+
+        while (continuar) {
+            exibirMenu();
+            int opcao = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcao) {
+                case 1:
+                    cadastrarAluno();
+                    break;
+                case 2:
+                    excluirAluno();
+                    break;
+                case 3:
+                    alterarAluno();
+                    break;
+                case 4:
+                    buscarAlunoPeloNome();
+                    break;
+                case 5:
+                    listarAlunos();
+                    break;
+                case 6:
+                    System.out.println("FIM.");
+                    continuar = false;
+                    break;
+                default:
+                    System.out.println("Opção inválida.Tente novamente.");
+            }
+        }
+        scanner.close();
+        entityManager.close();
+        entityManagerFactory.close();
+    }
+
+    private static void exibirMenu() {
+        System.out.println("**CADASTRO DE ALUNOS**");
+        System.out.println("1 - Cadastrar aluno");
+        System.out.println("2 - Excluir aluno");
+        System.out.println("3 - Alterar aluno");
+        System.out.println("4 - Buscar aluno pelo nome");
+        System.out.println("5 - Listar alunos");
+        System.out.println("6 - FIM");
+        System.out.print("Digite a opção desejada: ");
+    }
+
+    private static void cadastrarAluno() {
+        EntityManager em = JPAUtil.getEntityManager();
+
+        System.out.println("Digite o nome do aluno: ");
+        String nome = scanner.nextLine();
+        System.out.println("Digite o RA do aluno: ");
+        String ra = scanner.nextLine();
+        System.out.println("Digite o e-mail do aluno: ");
+        String email = scanner.nextLine();
+        System.out.println("Digite a nota 1 do aluno: ");
+        BigDecimal nota1 = scanner.nextBigDecimal();
+        System.out.println("Digite a nota 2 do aluno: ");
+        BigDecimal nota2 = scanner.nextBigDecimal();
+        System.out.println("Digite a nota 3 do aluno: ");
+        BigDecimal nota3 = scanner.nextBigDecimal();
+
+        AlunoDao alunoDao = new AlunoDao(em);
+        Aluno novoAluno = new Aluno(nome, ra, email, nota1, nota2, nota3);
+        em.getTransaction().begin();
+        alunoDao.cadastrar(novoAluno);
+        em.getTransaction().commit();
+        em.close();
+        System.out.println("Aluno cadastrado com sucesso!");
+    }
+
+    private static void excluirAluno() {
+
+    }
+
+    private static void alterarAluno() {
+
+    }
+
+    private static void buscarAlunoPeloNome() {
+
+    }
+
+    private static void listarAlunos() {
+
+    }
+}
